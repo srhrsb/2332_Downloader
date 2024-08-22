b270a84e77178c8ee721e255b935d6a08f2de6e6
srhrsb <sebastian.boettcher2@srh.de>
2024-08-22 11:44:26 +0200

    Download mit Thread

--- src/main/java/com/brh/downloader/Controller.java
+++ src/main/java/com/brh/downloader/Controller.java
@@ -3,6 +3,10 @@ package com.brh.downloader;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Controller {
     @FXML
@@ -11,11 +15,24 @@ public class Controller {
     @FXML
     protected void searchFolder() {
 
+        Stage stage  = (Stage) targetFolder.getScene().getWindow(); //Stage holen
+        DirectoryChooser directoryChooser = new DirectoryChooser(); //File Chooser Objekt erstellen
+        File selected = directoryChooser.showDialog( stage); //FileChooser öffnen
+        targetFolder.setText(selected.getAbsolutePath()); //gewählten Pfad an das Textfeld übergeben
 
     }
 
     @FXML
     protected void download() {
+         String link = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Blume_mit_Schmetterling_und_Biene_1uf.JPG/440px-Blume_mit_Schmetterling_und_Biene_1uf.JPG";
+        String link2 = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Blumenwiese2.jpg/440px-Blumenwiese2.jpg";
+
+        String target = targetFolder.getText();
+
+         if(!link.isEmpty() && !target.isEmpty())   {
+             new Thread( new Download( link, target) ).start();
+             new Thread( new Download( link2, target) ).start();
+         }
 
 
     }
