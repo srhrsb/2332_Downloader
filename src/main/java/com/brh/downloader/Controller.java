5c1a05ab7f10341059cb60335ee85ec7a6ca7e85
srhrsb <sebastian.boettcher2@srh.de>
2024-08-22 10:49:09 +0200

    Initial commit

---
+++ src/main/java/com/brh/downloader/Controller.java
@@ -0,0 +1,22 @@
+package com.brh.downloader;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private TextField targetFolder;
+
+    @FXML
+    protected void searchFolder() {
+
+
+    }
+
+    @FXML
+    protected void download() {
+
+
+    }
+}
---
+++ src/main/java/com/brh/downloader/Download.java
@@ -0,0 +1,16 @@
+package com.brh.downloader;
+
+public class Download {
+
+    private String url;
+    private int progress;
+
+    public Download(String url, String target) {
+        this.url = url;
+
+    }
+
+
+
+
+}
